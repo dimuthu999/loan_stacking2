124ad3297e521149139ef1b247f9e82b7679121d
Dimuthu Ratnadiwakara <dnratnadiwakara@bauer.uh.edu>
2017-01-29 20:42:25 -0600

    Jan 29 2017

--- lc_prosper_merge.R
+++ lc_prosper_merge.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 library(zoo)
 
-path = "E:/loan_stacking2"
+path = "E:/loan_stacking"
 setwd(path)
 trim <- function (x) {
   y = gsub("^\\s+|\\s+$", "", x)
@@ -13,10 +13,10 @@ trim <- function (x) {
 lc_raw = readRDS(file="E:/loan_stacking/lendingclub_raw.rds")
 lc_keep = c("id","member_id","loan_amnt","funded_amnt","term","int_rate","emp_length",
             "home_ownership","annual_inc","is_inc_v","accept_d","issue_d","addr_city","addr_state",
-            "dti","last_pymnt_d","len","fico","inq_last_6mths","delinq_amnt")
+            "dti","last_pymnt_d","len","fico","inq_last_6mths","delinq_amnt","open_acc")
 lc_keep_names = c("loan_id","borrower_id","loan_amount","pct_funded","loan_term","interest_rate","emp_length",
                   "home_ownership","annual_income","income_verified","accepted_date","issued_date","city","state",
-                  "dti","delinquent_amount","inq_last_6mths","last_payment_month","credit_history_months","fico")
+                  "dti","delinquent_amount","inq_last_6mths","open_credit_lines","last_payment_month","credit_history_months","fico")
 
 loandata <- lc_raw[,names(lc_raw) %in% lc_keep]
 names(loandata) <- lc_keep_names
@@ -45,8 +45,8 @@ prosper_raw <- prosper_raw[!duplicated(prosper_raw),]
 prosper_keep = c("LoanKey","MemberKey.x","LoanOriginalAmount","PercentFunded.x","Term","BorrowerRate.x","MonthsEmployed",
                  "IsBorrowerHomeowner","StatedMonthlyIncome.x","IncomeVerifiable.x","ListingCreationDate.y",
                  "LoanOriginationDate.x","BorrowerCity","BorrowerState.y","LoanCurrentDaysDelinquent","DebtToIncomeRatio",
-                 "FirstRecordedCreditLine.x","FICO","InquiriesLast6Months.x","AmountDelinquent.x")
-prosper_keep_names = c("loan_term","interest_rate","home_ownership","credit_history_months","inq_last_6mths","delinquent_amount","dti","income_verified",
+                 "FirstRecordedCreditLine.x","FICO","InquiriesLast6Months.x","AmountDelinquent.x","OpenCreditLines.x")
+prosper_keep_names = c("loan_term","interest_rate","home_ownership","credit_history_months","open_credit_lines","inq_last_6mths","delinquent_amount","dti","income_verified",
                        "annual_income","loan_id","last_payment_month","loan_amount","issued_date","borrower_id","pct_funded",
                        "accepted_date","emp_length","state","city","fico")
 loandata2 <- prosper_raw[,names(prosper_raw) %in% prosper_keep]
